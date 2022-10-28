f6e7af202c4d610b1fcd369f224d986a9c8c40cf
Dara <dvan1@utpl.edu.ec>
2022-10-28 11:24:47 -0500

    commit clase

--- ejemplos-IDE/Ejemplo03/src/ejemplo03/Ejemplo03.java
+++ ejemplos-IDE/Ejemplo03/src/ejemplo03/Ejemplo03.java
@@ -19,6 +19,12 @@ public class Ejemplo03 {
         
         // Tipo de datos Entero
         
+        /*
+        d
+        a
+        r
+        a
+        */
         int variable1;
         int variable2 = 10;
         int valorInicial = 18;
@@ -45,6 +51,12 @@ public class Ejemplo03 {
         
         boolean variable9 = false;
         boolean variable10 = true;
+        
+        boolean variable11 = false;
+        // boolean variable11 = false; // incorrecto
+        variable11 = false;
+        variable11 = true;
+        //variable11 = variable6; // incorrecto
     }
     
 }
